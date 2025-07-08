a16e32eb7826a79028548c0efa295022862f567e
Tim Gromeyer <tim.gromeyer@gmail.com>
2025-07-08 13:03:29 +0200

    Clean playstore-files branch

--- app/src/main/java/com/timgromeyer/tvkiller/IrBlaster.kt
+++
@@ -1,9 +0,0 @@
-package com.timgromeyer.tvkiller
-
-interface IrBlaster {
-    fun init(): Boolean
-    fun deinit()
-    fun sendIrSignal(frequency: Int, pattern: IntArray): Boolean
-    fun isAvailable(): Boolean
-    fun isReady(): Boolean // Added to check readiness
-}
